a381a4c7b0c0b61ffc934670129bd12f716f30f9
fadil dlz <fadil.dlz@gmail.com>
2020-12-20 22:17:16 +0700

    update tugas

---
+++ Tugas-Percabanga/looping.java
@@ -0,0 +1,12 @@
+public class looping {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++){
+            for (int j = 1; j <= i; j++){
+                if (j%2==1)
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
